b922fec7b138099393590a8697ac1946cc36280c
liming7616 <125348721+liming7616@users.noreply.github.com>
2025-06-21 16:38:21 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -111,9 +111,9 @@ const API_SITES = {
         name: 'iKun资源'
     },
     testSource: {
-        api: 'https://www.example.com/api.php/provide/vod',
-        name: '空内容测试源',
-        adult: true
+        api: 'https://jkunzyapi.com',
+        name: 'jkun资源',
+        adult: false
     },
     // 下面是一些成人内容的API源，默认隐藏，使用本项目浏览黄色内容违背项目初衷
     // 互联网上传播的色情内容将人彻底客体化、工具化，是性别解放和人类平等道路上的巨大障碍。
@@ -134,10 +134,10 @@ const API_SITES = {
     //     adult: true
     // },
     // bwzy: {
-    //     api: 'https://api.bwzym3u8.com',
-    //     name: '百万资源',
-    //     adult: true
-    // },
+         api: 'https://api.bwzym3u8.com',
+         name: '百万资源',
+        adult: false
+     },
     // souav: {
     //     api: 'https://api.souavzy.vip',
     //     name: 'souav资源',
